96d4cb59e0cb7acc9b21f101ae8f49167b1d8b63
Jubar Alam <jubaralam11022@gmail.com>
2024-09-05 23:31:57 +0530

    bash urlhas been updated

--- src/Components/context/BashURL.jsx
+++ src/Components/context/BashURL.jsx
@@ -3,7 +3,8 @@ import React, { createContext, useContext } from "react";
 const BashURLContext = createContext();
 
 const BashURLProvider = ({ children }) => {
-  const bashURL = "http://localhost:8080/api";
+  // const bashURL = "http://localhost:8080/api";
+  const bashURL = "https://kfc-backend-g0p5.onrender.com/api";
 
   return (
     <BashURLContext.Provider value={bashURL}>
